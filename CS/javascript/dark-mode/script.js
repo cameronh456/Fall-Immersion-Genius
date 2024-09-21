3121ce7980be0e3a51dd6d18736a1a29eeb6b433
cameronh456 <cameron.harris@hgs.hiddengeniusproject.org>
2024-09-20 17:54:57 -0700

    Update script.js

--- CS/javascript/dark-mode/script.js
+++ CS/javascript/dark-mode/script.js
@@ -1,3 +1,16 @@
 function darkMode() {
-// Your code here.
-}
+   // Get the body element
+    const body = document.body; 
+
+    // Write the code to toggle dark mode below, [Hint: use the style property of the body element]
+    if (body.style.backgroundColor === 'black') {
+// If it's black, change to light mode
+        body.style.backgroundColor = 'white';
+        body.style.color = 'black';
+    } else {
+        // If it's not black, change to dark mode
+        body.style.backgroundColor = 'black';
+        body.style.color = 'white';
+    }
+
+    }
