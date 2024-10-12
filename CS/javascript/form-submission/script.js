afb4be7071e34db918041ad0d3b504280b5d92db
cameronh456 <cameron.harris@hgs.hiddengeniusproject.org>
2024-10-11 17:41:38 -0700

    Update script.js

--- CS/javascript/form-submission/script.js
+++ CS/javascript/form-submission/script.js
@@ -1,3 +1,24 @@
 function formSubmission() {
     // Your code here.
 }
+document.addEventListener("DOMContentLoaded", () => {
+    const form = document.getElementById('myForm');
+
+form.addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    //Add Variab;es from index.html (hint: Use document.get function)
+
+    // Grabbing the values of the input fields from the form
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const message = document.getElementById('message').value;
+
+
+    if (name && email && message) {
+        alert(`Form submitted successfully!\nName: ${name}\nEmail: ${email}\nMessage: ${message}`);
+    } else {
+        alert('Please fill all the field');
+    }
+    });
+});
